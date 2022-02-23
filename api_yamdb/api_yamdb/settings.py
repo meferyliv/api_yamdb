c4c78b35c7a651088399baa0d0cb4438cfac0286
Ivan Lepskiy <mefery@gmail.com>
2022-02-23 17:05:02 +0300

    For first review

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -17,7 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'djoser',
+    'rest_framework_simplejwt',
     'django_filters',
     'api',
     'reviews',
